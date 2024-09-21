678db569e34650afced410b97110ccc5cd8efd41
umer-40492 <umer40492@gmail.com>
2024-09-21 13:48:41 +0500

    live in vercel

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,10 @@
+import Image from 'next/image'
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-        <img  className="h-52 " src="spinner.svg" alt="loading" />
+      <Image 
+      className="h-52 " src="spinner.svg" alt="loading" width={200} height={200}
+      />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 export default function footer() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-xl
-    justify-center gap-6'>
+    justify-center gap-6 fixed w-full bottom-0'>
       <Link href={'https://github.com/umer-40492'} target='#'> <FaGithub /></Link>
       <Link href={'https://www.linkedin.com/in/muhammad-umer-8b162720a/2'} target='#'> <FaLinkedin /></Link>
     </div>
